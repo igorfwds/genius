0bd6685a6bf83fc4dfff8dc4366056b96b5ef0b4
igor <igorfwds@hotmail.com>
2024-05-09 10:26:20 -0300

    function color_init done

--- genius.c
+++ genius.c
@@ -7,4 +7,20 @@ typedef struct color{
     color *next;
 }color;
 
+void color_init(color **head, color **tail,int arg_color);
+
+void color_init(color **head, color **tail,int arg_color){
+    color *new = (color*) malloc(sizeof(color));
+    if (new != NULL){
+        new->color_id = arg_color;
+        if(*head == NULL){
+            *head = new;
+        }else{
+            (*tail)->next = new;
+        }
+        *tail = new;
+        new->next = NULL;
+    }
+}
+
 
